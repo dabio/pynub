25e77fafecdec47a0d96402ad97742c1da4cd6f5
Danilo <dabio@users.noreply.github.com>
2018-03-04 22:15:06 +0100

    Initial import

---
+++ setup.py
@@ -0,0 +1,23 @@
+import io
+import os
+
+from setuptools import setup
+
+NAME = 'pinub'
+REQUIRES_PYTHON = '>=3.6.0'
+REQUIRED = [
+    'flask', 'psycopg2-binary'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'LICENSE'), encoding='utf-8') as f:
+    licence = '\n' + f.read()
+
+setup(
+    name=NAME,
+    py_modules='pinub',
+    python_requires=REQUIRES_PYTHON,
+    install_required=REQUIRED,
+    license=license
+)
